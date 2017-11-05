19fd2b02afe08686882a69c090ce3847317f5c2b
Baudoin Woods Ribadeau <baudoin.wr@gmail.com>
2017-11-05 11:04:37 +0100

    initial poc

---
+++ Assets/Scripts/RandomLight.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomLight : MonoBehaviour {
+
+    public GameObject lightSource;
+    SpriteRenderer sourceRenderer;
+    SpriteRenderer thisRenderer;
+    public float closeness = 0.05f;
+	// Use this for initialization
+	void Start () {
+        thisRenderer = gameObject.GetComponent<SpriteRenderer>();
+        thisRenderer.color = ColorPicker.generateNewColor();
+        sourceRenderer = lightSource.GetComponent<SpriteRenderer>();
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (closeEnough(thisRenderer.color, sourceRenderer.color, closeness))
+        {
+            Debug.Log("Matched!");
+            thisRenderer.color = ColorPicker.generateNewColor();
+        }
+    }
+
+    private bool closeEnough(Color c1, Color c2, float nearness)
+    {
+        return Mathf.Abs(c1.r - c2.r) < nearness
+            && Mathf.Abs(c1.g - c2.g) < nearness
+            && Mathf.Abs(c1.b - c2.b) < nearness;
+    }
+
+}
---
+++ Assets/Scripts/scrj.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class scrj : MonoBehaviour
+{
+    public GameObject lightGenerator;
+    public GameObject sin;
+    float previousPosition = 0.0f;
+    float previousPeak = 0.0f;
+    bool isGoingUp = true;
+
+    Queue<float> periods = new Queue<float>();
+    
+    // Use this for initialization
+    void Start()
+    {
+        periods.Enqueue(1);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    { 
+        if (Input.GetMouseButton(0))
+        {
+            float currentPosition = Input.mousePosition.y;
+            if (isGoingUp)
+            {
+                if (previousPosition > currentPosition)
+                {
+                    isGoingUp = false;
+                    periods.Enqueue(calculatePeriod());
+                }
+            }
+            else
+            {
+                if (previousPosition < currentPosition)
+                {
+                    isGoingUp = true;
+                    periods.Enqueue(calculatePeriod());
+                }
+            }
+            previousPosition = currentPosition;
+            float period = getAverage(periods.ToArray());
+            Color col = ColorPicker.GetColor(period);
+            lightGenerator.GetComponent<SpriteRenderer>().color = col;
+            sin.GetComponent<SinScript>().period = period;
+            sin.GetComponent<SinScript>().c2 = col;
+            //Camera.main.backgroundColor = col;
+            while (periods.ToArray().Length > 10)
+            {
+                periods.Dequeue();
+            }
+        }
+    }
+
+
+    private float getAverage(float[] arr)
+    {
+        float tot = 0;
+        foreach(float value in arr)
+        {
+            tot += value;
+        }
+        return tot / arr.Length;
+    }
+
+    private float calculatePeriod()
+    {
+        float currentTime = Time.time;
+        float period = currentTime - previousPeak;
+        previousPeak = currentTime;
+        return period;
+    }
+
+}
