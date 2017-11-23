d5539bc02b359e5dfcdc15f10d9fc169bac6a6aa
Baudoin Woods Ribadeau <baudoin.wr@gmail.com>
2017-11-23 22:26:49 +0100

    Shifting the light source to the game area. Adding a mode switch button.

--- Assets/Scripts/MainScripts/SwitchModeScript.cs
+++ Assets/Scripts/MainScripts/SwitchModeScript.cs
@@ -3,31 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwitchModeScript : MonoBehaviour
-{
-    void Update()
-    {
-        if (Input.touches.Length > 0)
-        {
-            Touch touch = Input.touches[0];
-            Ray touchRay = Camera.main.ScreenPointToRay(touch.position);
-            RaycastHit[] hits = Physics.RaycastAll(touchRay);
-            foreach (RaycastHit hit in hits)
-            {
-                if (hit.transform.gameObject == gameObject)
-                {
-                    SwitchChangingColor();
-                }
-            }
-        }
-    }
+public class SwitchModeScript : MonoBehaviour { 
 
-    void OnMouseDown()
-    {
-        SwitchChangingColor();
-   }
-
-    private void SwitchChangingColor()
+    public void SwitchChangingColor()
     {
         MainScript script = (MainScript)UnityEngine.Object.FindObjectOfType(typeof(MainScript));
         script.switchColorChanger();
